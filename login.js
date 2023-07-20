4d5b2f93d45ecdb2cf25d03f67f55d9a006b6161
mohorgoswami <prajnagoswami22@gmail.com>
2023-07-20 20:32:53 +0530

    add

---
+++ login.js
@@ -0,0 +1,18 @@
+
+        function addData(){
+            let username =document.getElementById('username').value
+            let password =document.getElementById('password').value
+
+            //localstorage
+
+            localStorage.setItem('username',username)
+            localStorage.setItem('password',password)
+
+             localStorage.username=username;
+             localStorage.password=pass;
+        }
+
+        
+
+        
+  
